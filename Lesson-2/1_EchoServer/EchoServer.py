b49e0fcc9d6556c1e0e6617b1a21413852be6af7
Christopher Aubuchon <aubuchcl@gmail.com>
2017-07-11 16:31:11 -0700

    changed EchoServer to deliver query text to browser

--- Lesson-2/1_EchoServer/EchoServer.py
+++ Lesson-2/1_EchoServer/EchoServer.py
@@ -19,7 +19,7 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 
-class HelloHandler(BaseHTTPRequestHandler):
+class EchoHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         # First, send a 200 OK response.
         self.send_response(200)
@@ -29,9 +29,12 @@ class HelloHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
         # Now, write the response body.
-        self.wfile.write("Hello, HTTP!\n".encode())
+        message = self.path[1:]
+        message_bytes = message.encode()
+        self.wfile.write(message_bytes)
+
 
 if __name__ == '__main__':
     server_address = ('', 8000)  # Serve on all addresses, port 8000.
-    httpd = HTTPServer(server_address, HelloHandler)
+    httpd = HTTPServer(server_address, EchoHandler)
     httpd.serve_forever()
